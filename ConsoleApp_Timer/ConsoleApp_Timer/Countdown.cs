7dc447033855c125c529b31da1a79004816606f9
OMAC-GBLZ <98odhranmcmanus@gmail.com>
2024-05-26 18:39:44 +0100

    Add files via upload

---
+++ ConsoleApp_Timer/ConsoleApp_Timer/Countdown.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Timer
+{
+    internal class Countdown
+    {
+
+        Timer timer;
+        Timer alarm;
+
+        public int Seconds { get; set; }
+        public int Minutes { get; set; }
+        public int Length;
+        public bool IsAlarmRinging = false;
+        public bool IsTimerRunning = false;
+
+        public Countdown(int minutes, int seconds)
+        {
+            Minutes = minutes;
+            Seconds = seconds;
+            Length = minutes * 60 + seconds;
+        }
+
+        public void StartTimer()
+        {
+            Console.WriteLine("Timer has started");
+            IsTimerRunning = true;
+            timer = new Timer(TimerCallback, null, 0, 1000);
+        }
+        private void TimerCallback(object o)
+        {
+
+            if (Length > 0)
+            {
+                Console.Clear();
+                Console.WriteLine($"{Length / 60} minutes {Length % 60}s remaining.\nPress enter at any time to cancel the timer.");
+                Length--;
+                GC.Collect();
+            }
+            else
+            {
+                Console.Clear();
+                EndTimer();
+            }
+        }
+
+        public void EndTimer()
+        {
+            IsTimerRunning = false;
+            timer.Dispose();
+            StartAlarm();
+        }
+
+        public void CancelTimer()
+        {
+            Console.Clear();
+            Console.Beep();
+            timer.Dispose();
+            Console.WriteLine($"Timer cancelled with {Length / 60} minutes {Length % 60}s remaining");
+        }
+
+        public void StartAlarm()
+        {
+            IsAlarmRinging = true;
+            alarm = new Timer(TimerCallback2, null, 0, 500);
+            Console.WriteLine($"{Length / 60} minutes {Length % 60}s remaining.\nPress enter at any time to stop the alarm.");
+        }
+
+        private void TimerCallback2(object p)
+        {
+            Console.Beep();
+            GC.Collect();
+        }
+
+        public void StopAlarm()
+        {
+            alarm.Dispose();
+            Console.WriteLine("Alarm stopped");
+            IsAlarmRinging = false;
+        }
+    }
+
+    
+}
---
+++ ConsoleApp_Timer/ConsoleApp_Timer/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Threading;
+using System.Threading.Channels;
+namespace ConsoleApp_Timer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutesInput = 0;
+            int secondsInput = 0;
+            
+            while (true)
+            {
+                Console.WriteLine("To start a timer, press enter. \nTo exit, enter \"1\".");
+
+                if (Console.ReadLine() == "1")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                }
+
+                bool success = false;
+                while (!success)
+                {
+                    Console.WriteLine("How many minutes would you like the timer to run for?");
+                    success = int.TryParse(Console.ReadLine(), out minutesInput);
+                    if (!success) 
+                    {
+                        Console.WriteLine("Please enter a valid number");
+                    }
+                }
+                
+                success = false;
+
+                while(!success)
+                {
+                   Console.WriteLine("How many additional seconds would you like the timer to run for?");
+                   success = int.TryParse(Console.ReadLine(), out secondsInput);
+                    if (!success)
+                    {
+                        Console.WriteLine("Please enter a valid number");
+                    }
+                }
+
+                Countdown countdown1 = new Countdown(minutesInput, secondsInput);
+
+                Console.WriteLine("Timer prepared for {0} minutes and {1} seconds, press enter to start the timer", countdown1.Length/60, countdown1.Length%60);
+                Console.ReadLine();
+
+                countdown1.StartTimer();
+
+                Console.WriteLine("Press enter for actions");
+                if (Console.ReadLine() != null)
+                {
+                    if (countdown1.IsTimerRunning == true)
+                    {
+                            countdown1.CancelTimer();
+                    }
+
+                    if (countdown1.IsAlarmRinging == true)
+                    {
+                            countdown1.StopAlarm();
+                    }
+                }
+            }
+        }
+    }
+}
